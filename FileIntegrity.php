7d280a6f04729670e8896783e9caaa2513478435
Kjell Heinsen <ich@kjell-heinsen.de>
2024-08-11 16:28:18 +0200

    FileIntegrity Class Part 2

--- FileIntegrity.php
+++ FileIntegrity.php
@@ -2,10 +2,12 @@
 
 class FileIntegrity {
 
-    private string $_root;
-    private string $_dir;
+    private ?string $_root = NULL;
+    private ?string $_dir = NULL;
 
-    private array $_hashes;
+    private ?array $_hashes = NULL;
+
+    private ?string $_filename = NULL;
 
  public function init():bool{
 
@@ -22,14 +24,14 @@ class FileIntegrity {
     /**
      * @return string
      */
- public function _getroot():string{
+ public function _getroot():?string{
      return $this->_root;
  }
 
     /**
      * @return string
      */
-    public function _getDir(): string
+    public function _getDir():?string
     {
         return $this->_dir;
     }
@@ -45,7 +47,7 @@ class FileIntegrity {
     /**
      * @return array
      */
-    public function _getHashes(): array{
+    public function _getHashes():?array{
         return $this->_hashes;
     }
 
@@ -56,14 +58,44 @@ class FileIntegrity {
     public function _setHashes(array $hashes):void{
         $this->_hashes = $hashes;
     }
- public function create():bool{
-    $dirname = $this->_getroot();
 
+    /**
+     * @return string|null
+     */
+    public function _getFilename():?string{
+        return $this->_filename;
+    }
+
+    /**
+     * @param string $filename
+     * @return void
+     */
+    public function _setFilename(string $filename):void{
+        $this->_filename = $filename;
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+ public function create():void{
+    $dirname = $this->_getroot();
     if(substr($this->_getroot(), -1) !== '/'){
         $dirname = $dirname . '/';
     }
     $dirname .= $this->_getDir();
+
+    $this->_setFilename($this->FileNameCreate());
     $this->_setHashes(FileHash::GetAll($dirname));
+
+    if(!file_exists($this->_getFilename())){
+       if(file_put_contents($this->_getFilename(),json_encode($this->_getHashes())) === false){
+           throw new \Exception('Datei kann nicht geschrieben werden.');
+       }
+    } else {
+        throw new \Exception('Datei existiert bereits.');
+    }
+
  }
 
 
